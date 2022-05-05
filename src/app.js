0040734a56acf9b199e1383f1641ae91530d19b3
mukesh kumar mukhiya <mukeshnmight@gmail.com>
2022-05-05 23:35:35 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const bcrypt = require("bcryptjs");
 const path = require("path");
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +12,7 @@ const hbs = require("hbs");
 const user_registration = require("./models/user_register");
 const async = require('hbs/lib/async');
 const { send } = require('process');
+const { table, Console } = require('console');
 
 
 const port = process.env.PORT || 3000;
@@ -23,6 +26,8 @@ app.set("view engine", "hbs");
 app.set("views", tamplate_path);
 hbs.registerPartials(partial_path);
 
+console.log("secret key is: ")
+console.log(process.env.SECRET_KEY);
 app.get('/', (req, res) => {
   res.status(200).render("index");
 
@@ -54,16 +59,16 @@ app.post('/registration', async (req, res) => {
       }
       )
 
+      const token = await registere_userdoc.generateAuthToken();
       const registered = await registere_userdoc.save();
       res.status(200).render("index");
-
     }
 
     else {
       res.status(201).send("Password is not matched");
     }
   } catch (error) {
-    res.status(404).send(error);
+    res.status(404).send("use different email ");
   }
 
 })
@@ -79,8 +84,10 @@ app.post('/login', async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     const userdtl = await user_registration.findOne({ email: email });
+    const ismatch = await bcrypt.compare(password, userdtl.password);
 
-    const ismatch = await bcrypt.compare(password, userdtl.password)
+    const token = await userdtl.generateAuthToken();
+    console.log("the login token is " + token);
 
     if (ismatch) {
       res.status(200).render("index");
@@ -93,6 +100,16 @@ app.post('/login', async (req, res) => {
   }
 })
 
+
+// const createToken = async () => {
+//   const token = await jwt.sign({ id: "62712520ae677283e75b493d" }, "mukeshmumar muksdfkshkfhjflksdjfljd", { expiresIn: "2 minutes" });
+//   console.log(token);
+//   console.log("something");
+//   const userverify = await jwt.verify(token, "mukeshmumar muksdfkshkfhjflksdjfljd");
+//   console.log(userverify);
+// }
+
+// createToken();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- src/models/user_register.js
+++ src/models/user_register.js
@@ -2,6 +2,7 @@ const async = require("hbs/lib/async");
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const user_register = new mongoose.Schema(
     {
@@ -44,21 +45,39 @@ const user_register = new mongoose.Schema(
             type: String,
             required: true,
             minlength: 3
-        }
-
+        },
+        tokens: [{
+            token: {
+                type: String,
+                required: true
+            }
+        }]
 
     }
 
 )
 
+user_register.methods.generateAuthToken = async function () {
+    try {
+        const token = await jwt.sign({ id: this.id.toString() }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({ token: token });
+        await this.save();
+        return token;
+    } catch (error) {
+        res.status(201).send("the eror is " + error);
+        console.log("error is : " + error);
+    }
+}
+
 user_register.pre("save", async function (next) {
     // const hassedPassword = await bcrypt.hash(password, 10);
     if (this.isModified("password")) {
-        console.log(`${this.password}`);
+        // console.log(`${this.password}`);
         this.password = await bcrypt.hash(this.password, 10);
         console.log(`${this.password}`);
 
-        this.cpassword = undefined;
+        this.cpassword = await bcrypt.hash(this.password, 10);
+        // this.cpassword = undefined;
     }
     next();
 })
